f3d7661ab65e2237db42520ca67c0319ce24aa7d
Daniil Raptanov <daniilraptanov@gmail.com>
2026-05-05 18:18:57 +0300

    add web sockets & configure prettier

---
+++ web-app/src/hooks/useWebSocket.js
@@ -0,0 +1,34 @@
+import { useMemo } from 'react';
+import useWebSocket, { ReadyState } from 'react-use-websocket';
+
+export function useESPWebSocket() {
+	const WS_URL = `ws://${window.location.hostname}/ws`;
+
+	const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket(
+		WS_URL,
+		{
+			shouldReconnect: () => true,
+			reconnectAttempts: 10,
+			reconnectInterval: 3000,
+			// onOpen: () => console.log('web socket opened'),
+			// onClose: () => console.log('web socket closed'),
+		}
+	);
+
+	const connectionStatus = useMemo(() => {
+		return {
+			[ReadyState.CONNECTING]: 'Connecting... ⏳',
+			[ReadyState.OPEN]: 'Connected 🟢',
+			[ReadyState.CLOSING]: 'Disconnecting... 🟡',
+			[ReadyState.CLOSED]: 'Disconnected 🔴',
+			[ReadyState.UNINSTANTIATED]: 'Waiting...',
+		}[readyState];
+	}, [readyState]);
+
+	return {
+		send: sendJsonMessage,
+		lastMessage: lastJsonMessage,
+		statusText: connectionStatus,
+		isConnected: readyState === ReadyState.OPEN,
+	};
+}
